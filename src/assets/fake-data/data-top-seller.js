8cffb3aa0ecadb31350942481d3bd39e2e91bb16
Natnael <natnaelgetenew@gmail.com>
2022-05-25 12:59:38 +0300

    minor fix

--- src/assets/fake-data/data-top-seller.js
+++ src/assets/fake-data/data-top-seller.js
@@ -1,91 +1,86 @@
-import img1 from '../images/avatar/avt-1.jpg'
-import img2 from '../images/avatar/avt-2.jpg'
-import img3 from '../images/avatar/avt-3.jpg'
-import img4 from '../images/avatar/avt-4.jpg'
-import img5 from '../images/avatar/avt-5.jpg'
-import img6 from '../images/avatar/avt-6.jpg'
-import img7 from '../images/avatar/avt-7.jpg'
-import img8 from '../images/avatar/avt-8.jpg'
-import img9 from '../images/avatar/avt-9.jpg'
-
-
-
+import img1 from "../images/avatar/avt-1.jpg";
+import img2 from "../images/avatar/avt-2.jpg";
+import img3 from "../images/avatar/avt-3.jpg";
+import img4 from "../images/avatar/avt-4.jpg";
+import img5 from "../images/avatar/avt-5.jpg";
+import img6 from "../images/avatar/avt-6.jpg";
+import img7 from "../images/avatar/avt-7.jpg";
+import img8 from "../images/avatar/avt-8.jpg";
+import img9 from "../images/avatar/avt-9.jpg";
 
 const topSellerData = [
-    {
-        img: img1,
-        name: "Guildwars 2 Gold / US" ,
-        price: "99 USD",
-        classPadding: ""
-    },
-    {
-        img: img2,
-        name: "WoW Gold / EU" ,
-        price: "60 EUR",
-        classPadding: "pl-17"
-    },
-    {
-        img: img4,
-        name: "WoW Gold / EU" ,
-        price: "65 EUR",
-        classPadding: "pl-34"
-    },
-    {
-        img: img5,
-        name: "Masterhorse Platinum Sword / EU" ,
-        price: "2000 EUR",
-        classPadding: "pl-51"
-    },
-    {
-        img: img6" ,
-        name: "Mastercrab Platinum Sword / EU" ,
-        price: "7000 EUR",
-        classPadding: "pl-68"
-    },
-    {
-        img: img8,
-        name: "Silver gloves WoW / KR" ,
-        price: "50000 KRW",
-        classPadding: ""
-    },
-    {
-        img: img6,
-        name: "Wow Gold / US" ,
-        price: "50 EUR",
-        classPadding: "pl-17"
-    },
-    {
-        img: img9,
-        name: "Level 80 Aion spell / US" ,
-        price: "5 USD",
-        classPadding: "pl-34"
-    },
-    {
-        img: img7,
-        name: "Blue Magic Drink / KR" ,
-        price: "3500 KRW",
-        classPadding: "pl-51"
-    },
-    {
-        img: img1,
-        name: "Golden horn magic spell / US" ,
-        price: "12 USD",
-        classPadding: "pl-68"
-    },
-    {
-        img: img1,
-        name: "Red magic spell pack of 20 / EU" ,
-        price: "15 EUR",
-        classPadding: ""
-    },
-    {
-        img: img2,
-        name: "Speed running spell / US" ,
-        price: "8.30",
-        classPadding: "pl-17"
-    },
-
-
-]
+  {
+    img: img1,
+    name: "Guildwars 2 Gold / US",
+    price: "99 USD",
+    classPadding: "",
+  },
+  {
+    img: img2,
+    name: "WoW Gold / EU",
+    price: "60 EUR",
+    classPadding: "pl-17",
+  },
+  {
+    img: img4,
+    name: "WoW Gold / EU",
+    price: "65 EUR",
+    classPadding: "pl-34",
+  },
+  {
+    img: img5,
+    name: "Masterhorse Platinum Sword / EU",
+    price: "2000 EUR",
+    classPadding: "pl-51",
+  },
+  {
+    img: "img6",
+    name: "Mastercrab Platinum Sword / EU",
+    price: "7000 EUR",
+    classPadding: "pl-68",
+  },
+  {
+    img: img8,
+    name: "Silver gloves WoW / KR",
+    price: "50000 KRW",
+    classPadding: "",
+  },
+  {
+    img: img6,
+    name: "Wow Gold / US",
+    price: "50 EUR",
+    classPadding: "pl-17",
+  },
+  {
+    img: img9,
+    name: "Level 80 Aion spell / US",
+    price: "5 USD",
+    classPadding: "pl-34",
+  },
+  {
+    img: img7,
+    name: "Blue Magic Drink / KR",
+    price: "3500 KRW",
+    classPadding: "pl-51",
+  },
+  {
+    img: img1,
+    name: "Golden horn magic spell / US",
+    price: "12 USD",
+    classPadding: "pl-68",
+  },
+  {
+    img: img1,
+    name: "Red magic spell pack of 20 / EU",
+    price: "15 EUR",
+    classPadding: "",
+  },
+  {
+    img: img2,
+    name: "Speed running spell / US",
+    price: "8.30",
+    classPadding: "pl-17",
+  },
+];
 
 export default topSellerData;
