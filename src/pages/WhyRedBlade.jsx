c104ca881967bc598b9f1e639228598b494185fe
TheRedBladeDAO <103996531+TheRedBladeDAO@users.noreply.github.com>
2022-06-08 16:16:45 +0200

    Update WhyRedBlade.jsx

--- src/pages/WhyRedBlade.jsx
+++ src/pages/WhyRedBlade.jsx
@@ -92,16 +92,20 @@ const WhyRedBlade = () => {
                         <div className="col-xl-6 col-md-12">
                             <div className="content-right">
                                 <div className="sc-item-details">
-                                    <h2 className="style2">Connect a crypto wallet </h2>
+                                    <h2 className="style2">Game Financing and investors </h2>
                                     <h5>
 
-                                    Stablecoins are the new mantra and allow you to hedge against volatility in the market. 
-
-But we all know this is not the case anymore. If you deal with crypto currencies, there is constant and high volatility and there is oftentimes a lack of underlying value and assets. However, with the Red Blade token you have a unique token that has an underlying asset: the game items placed on our exchange. We pay for and also hold items that are already traded on various game item exchanges worldwide with verified customer transactions and verified prices. We also track the prices of these assets in order to manage the value of our treasury.
+                                    Crypto assets should be stable. Nobody wants to worry about his savings and investments. As a crypto investor you want to avoid 
+        high volatility and benefit from the growth of the ecosystem that you support with your token purchase.</h4><br/>
+        
+        The Red Blade DAO is providing this necessary stability through its treasury and the underlying game assets which it holds. 
+        With the Red Blade token you therefore have a unique token whose value is secured by widely traded game-items, currencies, characters and accounts. 
+        Our DAO will invests in and also holds these items over a certain period while tracking their value, prices and exchange-rates on the various worlwide 
+        item exchanges. 
 </h5><br/><br/>
 <h4>Not just for investors, but also gamers</h4><br/>
 <h6>
-We are a multidimensional project. First of all, our token is a utility for gamers. They can benefit from a new financial model for their inventory. They also benefit from a easier connectivity into the newly evolving world of crypto games. While there is scepticism against some forms of blockchain-based games and NFTs, we are dealing with an unstoppable force. Accordingly, we want to allow gamers to utilize this trends to their advantage. 
+We are a multidimensional project. First of all, our token is a utility for gamers. Game-players can benefit from a new financial model for their inventory. They also benefit from a easier connectivity into the newly evolving world of crypto games. While there is scepticism against some forms of blockchain-based games and NFTs, we are dealing with an unstoppable force. Accordingly, we want to allow gamers to utilize this trends to their advantage. 
 <br />
 We also see other gaming platforms and ecoystems pushing into the blockchain world. Here we see ourselves as neutral: we are planning to integrate any game-title or item to be published or traded through our platform. We also except other gaming crypto currencies and plan to integrate these as well, assuming we can arrange benefits for our gamers. 
 <br /><br />
