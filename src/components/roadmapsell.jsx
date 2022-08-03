c5389165e66b3e72cf05bfdde2bdb95a9d01a2cb
Patrick Reinbold <reinpa@outlook.de>
2022-08-03 11:22:16 +0300

    remove borders and go back to old tokensale look

--- src/components/roadmapsell.jsx
+++ src/components/roadmapsell.jsx
@@ -29,81 +29,94 @@ const RoadMapSell = () => {
 <hr />
 
 
-            <div className="tf-section tf-item-details pb-5">
+            <div className="tf-section tf-item-details">
 
             <div className="heading mg-bt-12">
                                 <h2 className="tf-title">Join the $MXDT Token PreSales!</h2>
                             </div>
 
-                <div className="themesflat-container mb-5">
+                <div className="themesflat-container">
 
 
 
 
 
 
-  <div className="container">
+                <div className="container-fluid">
+  <div className="container p-5">
     <div className="row d-flex justify-content-center">
-
       <div className="col-lg-3 col-md-12 mb-4">
-        <div className="card text-center my-3">
-          <div className="card-header">
-            Pre-Sale
-          </div>
+        <div className="card card1 h-100 p-5 tf-section">
           <div className="card-body">
-            <h5 className="card-title">0.01 USDC</h5>
-            <p className="card-text">Whitelist only<br />
-              Sold out!</p>
+            <h3 className="card-title">Pre-Sale</h3>
+            <br />
+            <br />
+            <span className="h2">Price: 0.01 USDC</span>
+            <br />
+            <br />
+            <div className="d-grid my-3">
+              {/* <a href='https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137' className="btn btn-outline-dark btn-block">Buy Now</a> */}
+            </div>
+            <ul>
+              <li>Whitelist only</li>
+              <li>Sold out!</li>
+            </ul>
           </div>
-        </div>  
+        </div>
       </div>
-
       <div className="col-lg-3 col-md-12 mb-4">
-        <div className="card text-center my-3">
-          <div className="card-header">
-            Pre-Sale 1
-          </div>
+        <div className="card card2 h-100 p-5 tf-section">
           <div className="card-body">
-            <h5 className="card-title">0.02 USDC</h5>
-            <p className="card-text"></p>
-            <a href="https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137" target="_blank" className="btn btn-primary">Buy Now</a>
+          <h3 className="card-title">Pre-Sale 1</h3>
+            <br />
+            <br />
+            <span className="h2 tf-title">Price: 0.02 USDC</span>
+            <br />
+            <br />
+            <div className="d-grid my-3">
+            <a href='https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137' target="_blank" className="btn btn-outline-dark btn-block buy-a">Buy Now</a>
+            </div>
+            <ul>
+              <li>Start: August 1 - 10</li>
+            </ul>
           </div>
-          <div className="card-footer text-muted">
-          Start: August 1 - 10
-          </div>
-        </div> 
+        </div>
       </div>
-
       <div className="col-lg-3 col-md-12 mb-4">
-        <div className="card text-center my-3">
-          <div className="card-header">
-            Pre-Sale 2
-          </div>
+        <div className="card card3 h-100 p-5 tf-section">
           <div className="card-body">
-            <h5 className="card-title">0.02 USDC</h5>
-            <p className="card-text"></p>
-            <a href="https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137" target="_blank" className="btn btn-primary">Coming Soon</a>
+          <h3 className="card-title">Pre-Sale 2</h3>
+            <br />
+            <br />
+            <span className="h2">Price: 0.02 USDC</span>
+            <br />
+            <br />
+            <div className="d-grid my-3">
+            <a href='https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137' target="_blank" className="btn btn-outline-dark btn-block buy-a">Coming Soon</a>
+            </div>
+            <ul>
+              <li>Start: August 15 - 22</li>
+            </ul>
           </div>
-          <div className="card-footer text-muted">
-          Start: August 15 - 22
-          </div>
-        </div> 
+        </div>
       </div>
-
       <div className="col-lg-3 col-md-12 mb-4">
-        <div className="card text-center my-3">
-          <div className="card-header">
-            IDO
-          </div>
+        <div className="card card3 h-100 p-5 tf-section">
           <div className="card-body">
-            <h5 className="card-title">0.08 USDC</h5>
-            <p className="card-text"></p>
-            <a href="https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137" target="_blank" className="btn btn-primary">Coming Soon</a>
+          <h3 className="card-title">IDO</h3>
+            <br />
+            <br />
+            <span className="h2">Price: 0.08 USDC</span>
+            <br />
+            <br />
+            <div className="d-grid my-3">
+            <a href='https://app.sushi.com/miso/0xAc6d5AeF8F56177f0f882cE1123F8AD1737f9601?chainId=137' target="_blank" className="btn btn-outline-dark btn-block buy-a">Coming Soon</a>
+            </div>
+            <ul>
+              <li>Start: October 1 - 15</li>
+            </ul>
           </div>
-          <div className="card-footer text-muted">
-            Start: October 1 - 15
-          </div>
-        </div> 
+        </div>
       </div>
     </div>
   </div>
@@ -111,6 +124,9 @@ const RoadMapSell = () => {
 
 
 
+
+
+                </div>
             </div>
 
 
