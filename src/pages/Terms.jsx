84609864aac0076cae0048842a68bc1121fe12ab
TheRedBladeDAO <103996531+TheRedBladeDAO@users.noreply.github.com>
2022-08-29 13:02:41 +0200

    Update Terms.jsx
    added contacts

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -46,8 +46,10 @@ const Terms = () => {
 
 <p>Welcome to The Metaverse Exchange DAO !</p>
 
-<p>These terms and conditions outline the rules and regulations for the use of The Metaverse Exchange DAO's Website, located at https://theredbladedao.netlify.app/.</p>
+<p>These terms and conditions outline the rules and regulations for the use of The Metaverse Exchange DAO's Website, located at https://www.metaverse-exchange.xyz.</p>
 
+<p>If you have any questions regarding our DAO, platform-service or the $MXDT token, please register with our Discord community and ask for support. You can also contact us by email at mxdao@proton.me</p>
+        
 <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use The Metaverse Exchange DAO  if you do not agree to take all of the terms and conditions stated on this page.</p>
 
 <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
