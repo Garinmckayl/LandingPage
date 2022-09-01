8777abd931c3230389f500873b939695f663e23d
TheRedBladeDAO <103996531+TheRedBladeDAO@users.noreply.github.com>
2022-09-01 18:58:33 +0200

    Update data-slider-3.js

--- src/assets/fake-data/data-slider-3.js
+++ src/assets/fake-data/data-slider-3.js
@@ -11,27 +11,27 @@ const heroSliderData = [
     title_2: "any item, skin or currency",
     title_3: "from your favorite games",
     description:
-      "New financial liquidity for gamers powered by a DAO",
+      "We use blockchain technology to trade faster.",
     // img: img1,
     imgbg: imgbg1,
     class: "left",
   },
   {
     title_1: "Discover and monetize",
-    title_2: "NFTs and scholarships",
+    title_2: "Game NFTs and Scholarships",
     title_3: "with the $MXDT token",
     description:
-      "Receive and assign game scholarships at fair rates",
+      "Receive game scholarships at fair rates or become a scholarhip sponsor.",
     // img: img1,
     imgbg: imgbg2,
     class: "left",
   },
   {
-    title_1: "Triple AAA players",
-    title_2: "buy and sell",
+    title_1: "The Marketplace for Web2 and Web3",
+    title_2: "Buy and Sell",
     title_3: "skins, game items, currencies",
     description:
-      "Enjoy the financial benefits you have created with your game history",
+      "Quickly trade any ingame item quickly with crypto currencies",
     // img: img2,
     imgbg: imgbg3,
     class: "right",
