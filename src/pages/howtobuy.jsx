cf54424d1fa35caa719ed27321f515665b485cae
TheRedBladeDAO <103996531+TheRedBladeDAO@users.noreply.github.com>
2022-09-08 09:45:46 +0200

    Update howtobuy.jsx

--- src/pages/howtobuy.jsx
+++ src/pages/howtobuy.jsx
@@ -15,17 +15,17 @@ const Howtobuy = () => {
             {
                 key: "1",
                 title: 'How do I receive a scholarship?',
-                text: 'Go to your account, then choose "scholarship" and fill out the form. We will review the application and contact you by email. Please note that you have to be a MXDT tokenholder to qualify for scholarhips.'
+                text: 'Right now, we are still building out our platform, but you can sign-up to the applicant list. You can move up the waitlist by inviting people to our Discord community. Our scholarships will be assigned, as soon as the platform goes live by providing a detailed process within your user-acount.'
             },
             {
                 key: "2",
                 title: 'How many games do you support?',
-                text: 'This is completely up to the community, as our platform does not limit the number of kind of games and items. As long as it is a legal trade and there is a buyer or seller for a specific game item or currency, the game will be part of our platform and community.'
+                text: 'Our platform does not limit the number of kind of games, items, skins, NFTs or currencies. As long as it is a legal trade and there is a buyer or seller for a specific game item or currency, the game will be part of our platform and community.'
             },
             {   key: "3",
                 show: "show",
-                title: 'How do I trade an ingame asset, skin or NFT?',
-                text: 'To buy an item, first connect your crypto wallet or register . Then click on the item you want to buy, execute the payment and wait for the item to be handed over to you. The hand-over can be either in-game or - if blockchain-based - directly into your wallet. To sell an item, you have to fill out the creation form under sell-item, and then wait for a buyer.'
+                title: 'Why should I trade an ingame asset, skin or NFT?',
+                text: 'We are the Ebay of item-trading, but we use blockchain technology. Similar to old books or clothes you can sell on Ebay, you can convert unused game items, skins or NFTs into money by using our platform. To be safe, we will offer a 30-day return guarantee if requested.'
             },
             {
                 key: "4",
@@ -60,7 +60,7 @@ const Howtobuy = () => {
             {
                 key: "10",
                 title: 'What is a DAO anyway?',
-                text: 'A DAO is a Decentralized Autonomous Organization. This means that the organization is set up in a decentralized way with most decisions being my by software written in smartcontracts. These smartcontracts can be developed in a way that it is impossible for humans to change decisions in a subjective or personal direction. Intead the smartcontracts are predefined and will be continuously be executed, e.g. how many tokens should be circulated, how many scholarships should be granted. More information on this is given in our Whitepaper especially in regards to the which sort of decisions are taken by token holders.'
+                text: 'A DAO is a Decentralized Autonomous Organization. This means that the organization is set up in a decentralized way with most decisions being made my by software written in smartcontracts. These smartcontracts can be developed in a way that it is impossible for humans to change decisions in a subjective or personal direction. Intead the smartcontracts are predefined and will be continuously be executed, e.g. how many tokens should be circulated, how many scholarships should be granted. More information on this is given in our Whitepaper especially in regards to the which sort of decisions are taken by token holders.'
             },
             
 
