e1f73ab5f577f89f0565e1f20e9f5c43d719176a
Natnael <natnaelgetenew@gmail.com>
2022-10-17 22:41:24 +0300

    width fix

--- src/pages/howtobuy.jsx
+++ src/pages/howtobuy.jsx
@@ -131,8 +131,8 @@ const Howtobuy = () => {
 
 </div>
 
-<div
-  className="row no-gutters clients-wrap clearfix wow fadeInUp howto-container"
+{/* <div
+  className="row no-gutters clients-wrap clearfix wow fadeInUp howto-container align-center justify-content-center mb-4"
   style={{ visibility: "visible", animationName: "fadeInUp" }}
 >
   <div className="col-lg-3 col-md-4 col-xs-6">
@@ -169,7 +169,7 @@ const Howtobuy = () => {
     <div className="client-logo">
       <img
         src="https://www.geckoterminal.com/_next/image?url=%2Fimages%2Flogo_dark.png&w=256&q=75"
-        className=""
+        className="gecko"
         width={200} height={150}
         alt=""
       />
@@ -177,7 +177,75 @@ const Howtobuy = () => {
   </div>
 
 
-</div>
+</div> */}
+
+
+<section className="features3 cid-qKT6knwV2G" id="clients2-2p">
+  <div className="container mb-4">
+    <div className="media-container-row">
+      <div className="col-12 align-center">
+        <h2 className="mbr-section-title pb-2 mbr-fonts-style display-2">
+          On
+        </h2>
+        <h3 className="mbr-section-subtitle mbr-light mbr-fonts-style display-7">
+        </h3>
+      </div>
+    </div>
+  </div>
+  <div className="container">
+    <div className="row align-center justify-content-center">
+      <div className="p-3 col-12 col-md-6 col-lg-3">
+        <div className="client-wrapper">
+          <div className="wrap-img client-logo">
+            <img
+              src="https://uploads-ssl.webflow.com/5f9a1900790900e2b7f25ba1/60f6c47070e683cb60fd647f_Sushiswap.png"
+              className="img-responsive clients-img"
+            />
+          </div>
+          <div className="card-box">
+          </div>
+        </div>
+      </div>
+      <div className="p-3 col-12 col-md-6 col-lg-3">
+        <div className="client-wrapper">
+          <div className="wrap-img client-logo">
+            <img
+              src="https://branditechture.agency/brand-logos/wp-content/uploads/wpdm-cache/Polygon-MATIC-900x0.png"
+              className="img-responsive clients-img"
+            />
+          </div>
+          <div className="card-box">
+          </div>
+        </div>
+      </div>
+      <div className="p-3 col-12 col-md-6 col-lg-3">
+        <div className="client-wrapper">
+          <div className="wrap-img client-logo">
+            <img
+              src="https://gopluslabs.io/images/plus2/about_logo.png"
+              className="img-responsive clients-img"
+            />
+          </div>
+          <div className="card-box">
+          </div>
+        </div>
+      </div>
+      <div className="p-3 col-12 col-md-6 col-lg-3">
+        <div className="client-wrapper">
+          <div className="wrap-img client-logo">
+            <img
+              src="https://www.geckoterminal.com/_next/image?url=%2Fimages%2Flogo_dark.png&w=256&q=75"
+              className="img-responsive clients-img"
+            />
+          </div>
+          <div className="card-box">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
             </section>
             <Footer />
